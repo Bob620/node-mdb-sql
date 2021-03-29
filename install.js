6360ad354d4b819262007533b8eac0d1a86efa55
bob620 <bruder.kraft225@gmail.com>
2021-03-29 13:18:12 -0500

    initial commit

---
+++ install.js
@@ -0,0 +1,25 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const bent = require('bent')('buffer');
+const extract = require('extract-zip');
+
+const pack = require('./package.json');
+
+async function get(uri) {
+	try {
+		await fs.writeFile('./prebuilt.zip', await bent(uri));
+		await extract('./prebuilt.zip', {
+			dir: path.resolve('./')
+		});
+		await fs.rm('./prebuilt.zip');
+	} catch(err) {
+		if (err.statusCode === 302)
+			return await get(err.headers.location);
+		console.log(err);
+	}
+}
+
+const uri = `https://github.com/Bob620/node-mdb-sql/releases/download/v${pack.version}/${process.platform}-mdb-sql-prebuilt.zip`;
+console.log(`Downloading precompiled mdb-sql at '${uri}'`);
+get(uri);
